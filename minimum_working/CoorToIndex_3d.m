b18716cdf4c23ce24d6cf20f52d01ab28d4f0a3c
ienore <ienore@mail.ustc.edu.cn>
2020-11-14 08:36:48 +0800

    Revert "Minimum Requirement"
    This reverts commit 324a1c261afc4a17696e852199088faf3a0e957b.

--- minimum_working/CoorToIndex_3d.m
+++
@@ -1,4 +0,0 @@
-function index = CoorToIndex_3d(x,y,z,NumInEdge)
-    index = NumInEdge^2*(z-1) + NumInEdge*(y-1)+x;
-end
-
--- minimum_working/IndexToCoor_3d.m
+++
@@ -1,6 +0,0 @@
-function [x,y,z] = IndexToCoor_3d(index,NumInEdge)
-    x = mod(index-1,NumInEdge)+1;
-    y = mod((index-x)/NumInEdge,NumInEdge)+1;
-    z = (index - x - NumInEdge*(y-1))/NumInEdge^2+1;
-end
-
